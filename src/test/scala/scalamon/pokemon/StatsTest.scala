b11edd21654a82b07dd40b9a30d78e1eb9cf6e4d
Federico Brighi <federico.brighi2@studio.unibo.it>
2026-06-04 18:05:50 +0200

    [PB-01] Added Stats to domain model with TDD validation

---
+++ src/test/scala/scalamon/pokemon/StatsTest.scala
@@ -0,0 +1,27 @@
+package scalamon.pokemon
+
+import scalamon.domain.pokemon.Stats
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.*
+
+class StatsTest extends AnyFunSuite:
+
+  test("Stats should be created correctly with valid positive values") {
+    val myStats = Stats(hp = 39, attack = 52, defense = 43, specialAttack = 60, specialDefense = 50, speed = 65)
+
+    myStats.hp shouldBe 39
+    myStats.attack shouldBe 52
+    myStats.defense shouldBe 43
+    myStats.specialAttack shouldBe 60
+    myStats.specialDefense shouldBe 50
+    myStats.speed shouldBe 65
+  }
+
+  test("Stats creation should fail if HP is 0 or a negative value") {
+    a[IllegalArgumentException] should be thrownBy Stats(hp = 0, attack = 52, defense = 43, specialAttack = 60, specialDefense = 50, speed = 65)
+  }
+
+  test("Stats creation should fail if at least one of the other statistics is a negative value") {
+    a[IllegalArgumentException] should be thrownBy Stats(hp = 39, attack = -1, defense = 43, specialAttack = 60, specialDefense = 50, speed = 65)
+    a[IllegalArgumentException] should be thrownBy Stats(hp = 39, attack = -1, defense = 0, specialAttack = -60, specialDefense = 0, speed = -65)
+  }
