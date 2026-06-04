4ce1889180fd890830ee5dbfcfc7c9caa9aba984
Mattia Saponaro <mattia.saponaro2@studio.unibo.it>
2026-06-04 17:13:49 +0200

    Added the TypeChart and it's tests

---
+++ src/main/scala/scalamon/domain/types/TypeChart.scala
@@ -0,0 +1,28 @@
+package scalamon.domain.types
+
+object TypeChart:
+  import Type.*
+  import TypeEffectiveness.*
+
+  private val nonNeutralMatchUps: Map[(Type, Type), TypeEffectiveness] = Map(
+    (Fire, Grass) -> SuperEffective,
+    (Fire, Water) -> NotVeryEffective,
+    (Fire, Fire) -> NotVeryEffective,
+    (Water, Fire) -> SuperEffective,
+    (Water, Grass) -> NotVeryEffective,
+    (Water, Water) -> NotVeryEffective,
+    (Grass, Water) -> SuperEffective,
+    (Grass, Fire) -> NotVeryEffective,
+    (Grass, Grass) -> NotVeryEffective,
+    (Grass, Poison) -> NotVeryEffective,
+    (Electric, Water) -> SuperEffective,
+    (Electric, Grass) -> NotVeryEffective,
+    (Electric, Electric) -> NotVeryEffective,
+    (Psychic, Poison) -> SuperEffective,
+    (Psychic, Psychic) -> NotVeryEffective,
+    (Poison, Grass) -> SuperEffective,
+    (Poison, Poison) -> NotVeryEffective
+  )
+
+  def effectiveness(attacking: Type, defending: Type): TypeEffectiveness =
+    nonNeutralMatchUps.getOrElse((attacking, defending), Neutral)
