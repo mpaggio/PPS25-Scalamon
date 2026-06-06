366f83fe750139bba480b7672c3794bd6fe58ae0
Marco Paggetti <marco.paggetti@studio.unibo.it>
2026-06-06 12:38:08 +0200

    Added Power to Move trait

--- src/main/scala/scalamon/domain/moves/Move.scala
+++ src/main/scala/scalamon/domain/moves/Move.scala
@@ -1,15 +1,16 @@
 package scalamon.domain.moves
 
 import Accuracy.*
+import Power.*
 
 sealed trait Move:
   def name: String
-  def power: Int
+  def power: Power
   def pp: Int
   def accuracy: Accuracy
 
-case class DamageMove(name: String, power: Int, pp: Int, accuracy: Accuracy) extends Move
+case class DamageMove(name: String, power: Power, pp: Int, accuracy: Accuracy) extends Move
 
-case class StatusMove(name: String, power: Int, pp: Int, accuracy: Accuracy) extends Move
+case class StatusMove(name: String, power: Power, pp: Int, accuracy: Accuracy) extends Move
 
 // object Move per la factory, tipi opachi legati più strettamente alla mossa e logica comune
--- src/test/scala/scalamon/domain/moves/MoveTest.scala
+++ src/test/scala/scalamon/domain/moves/MoveTest.scala
@@ -2,19 +2,20 @@ package scalamon.domain.moves
 
 import org.scalatest.matchers.should.Matchers.*
 import Accuracy.*
+import Power.*
 
 class MoveTest extends org.scalatest.funsuite.AnyFunSuite:
 
   test("Moves should be of two possible types (damage and status)"):
     """import scalamon.domain.moves.*
-      |DamageMove("Thunder", 120, 16, fromPercent(70))""".stripMargin should compile
+      |DamageMove("Thunder", fromInt(120), 16, fromPercent(70))""".stripMargin should compile
     """import scalamon.domain.moves.*
-      |scalamon.domain.moves.StatusMove("Thunder wave", 0, 32, fromPercent(90))""".stripMargin should compile
+      |scalamon.domain.moves.StatusMove("Thunder wave", fromInt(0), 32, fromPercent(90))""".stripMargin should compile
 
   test("Damage moves should have name, power, pp and accuracy"):
-    val testedMove: Move = DamageMove("Thunder", 120, 16, fromPercent(70))
+    val testedMove: Move = DamageMove("Thunder", fromInt(120), 16, fromPercent(70))
     testedMove.name shouldBe "Thunder"
-    testedMove.power shouldBe 120
+    testedMove.power shouldBe fromInt(120)
     testedMove.pp shouldBe 16
     testedMove.accuracy shouldBe fromPercent(70)
 
@@ -22,12 +23,12 @@ class MoveTest extends org.scalatest.funsuite.AnyFunSuite:
     assertDoesNotCompile("""DamageMove("Thunder", 1.0)""")
 
   test("Moves should have a name and accuracy"):
-    val testedMove: Move = DamageMove("Thunder", 120, 16, fromPercent(70))
+    val testedMove: Move = DamageMove("Thunder", fromInt(120), 16, fromPercent(70))
     testedMove.name shouldBe "Thunder"
     testedMove.accuracy shouldBe fromPercent(70)
     testedMove.accuracy.asString shouldBe "Accuracy: 70%"
 
   test("Moves should have an accuracy between 0.0 and 1.0"):
-    assertThrows[IllegalArgumentException](DamageMove("Thunder", 120, 16, fromRatio(1.5)))
-    assertThrows[IllegalArgumentException](DamageMove("Thunder", 120, 16, fromRatio(-0.5)))
-    noException should be thrownBy DamageMove("Thunder", 120, 16, fromRatio(0.7))
+    assertThrows[IllegalArgumentException](DamageMove("Thunder", fromInt(120), 16, fromRatio(1.5)))
+    assertThrows[IllegalArgumentException](DamageMove("Thunder", fromInt(120), 16, fromRatio(-0.5)))
+    noException should be thrownBy DamageMove("Thunder", fromInt(120), 16, fromRatio(0.7))
