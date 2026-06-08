4c3dfe0fe3f5fbb92aa5007a256a85dde00c5913
Marco Paggetti <marco.paggetti@studio.unibo.it>
2026-06-08 10:53:50 +0200

    Added all stats to Move, apart from Effect

--- src/main/scala/scalamon/domain/moves/Move.scala
+++ src/main/scala/scalamon/domain/moves/Move.scala
@@ -3,15 +3,33 @@ package scalamon.domain.moves
 import Accuracy.*
 import Power.*
 import PowerPoints.*
+import scalamon.domain.types.Type
+
+enum MoveCategory:
+  case Physical,Special
 
 sealed trait Move:
   def name: String
-  def power: Power
   def pp: PP
   def accuracy: Accuracy
+  def moveType: Type
 
-case class DamageMove(name: String, power: Power, pp: PP, accuracy: Accuracy) extends Move
+sealed trait DamagingMove extends Move:
+  def power: Power
+  def category: MoveCategory
 
-case class StatusMove(name: String, power: Power, pp: PP, accuracy: Accuracy) extends Move
+case class StatusMove(
+  name: String,
+  pp: PP,
+  accuracy: Accuracy,
+  moveType: Type) extends Move
+
+case class DamageMove(
+  name: String,
+  power: Power,
+  pp: PP,
+  accuracy: Accuracy,
+  moveType: Type,
+  category: MoveCategory) extends DamagingMove
 
 // object Move per la factory, tipi opachi legati più strettamente alla mossa e logica comune
--- src/test/scala/scalamon/domain/moves/MoveTest.scala
+++ src/test/scala/scalamon/domain/moves/MoveTest.scala
@@ -1,39 +1,46 @@
 package scalamon.domain.moves
 
 import org.scalatest.matchers.should.Matchers.*
-import Accuracy.*
 import Power.*
 import PowerPoints.*
+import Accuracy.*
+import scalamon.domain.moves.MoveCategory.*
+import scalamon.domain.types.Type.*
 
 class MoveTest extends org.scalatest.funsuite.AnyFunSuite:
 
   test("Moves should be of two possible types (damage and status)"):
     """import scalamon.domain.moves.*
-      |DamageMove("Thunder", powerFromInt(120), powerPointsFromInt(16), accuracyFromPercent(70))""".stripMargin should compile
+      |DamageMove("Thunder", powerFromInt(120), powerPointsFromInt(16), accuracyFromPercent(70), Electric, Special)""".stripMargin should compile
     """import scalamon.domain.moves.*
-      |scalamon.domain.moves.StatusMove("Thunder wave", powerFromInt(0), powerPointsFromInt(32), accuracyFromPercent(90))""".stripMargin should compile
+      |scalamon.domain.moves.StatusMove("Thunder wave", powerPointsFromInt(32), accuracyFromPercent(90), Electric)""".stripMargin should compile
 
   test("Damage moves should have name, power, pp and accuracy"):
-    val testedMove: Move = DamageMove("Thunder", powerFromInt(120), powerPointsFromInt(16), accuracyFromPercent(70))
+    val testedMove: DamagingMove = DamageMove("Thunder", powerFromInt(120), powerPointsFromInt(16), accuracyFromPercent(70), Electric, Special)
     testedMove.name shouldBe "Thunder"
     testedMove.power shouldBe powerFromInt(120)
     testedMove.pp shouldBe powerPointsFromInt(16)
     testedMove.accuracy shouldBe accuracyFromPercent(70)
+    testedMove.moveType shouldBe Electric
+    testedMove.category shouldBe Special
 
-  test("Status moves should have a name, power, pp and accuracy"):
-    val testedMove: Move = StatusMove("Thunder", powerFromInt(120), powerPointsFromInt(16), accuracyFromPercent(70))
+  test("Status moves should have a name, pp and accuracy"):
+    val testedMove: Move = StatusMove("Thunder", powerPointsFromInt(16), accuracyFromPercent(70), Electric)
     testedMove.name shouldBe "Thunder"
-    testedMove.power.asString shouldBe "Power: 120"
     testedMove.pp.asString shouldBe "PP: 16"
     testedMove.accuracy.asString shouldBe "Accuracy: 70%"
+    testedMove.moveType shouldBe Electric
 
-  test("Move accuracy should not be created without a value of type Power, a value of type PP and a value of type Accuracy"):
-    assertDoesNotCompile("""DamageMove("Thunder", 120, 16, 70.0)""")
-    assertDoesNotCompile("""DamageMove("Thunder", powerFromInt(120), powerPointsFromInt(16), 70.0)""")
-    assertDoesNotCompile("""DamageMove("Thunder", 120, powerPointsFromInt(16), accuracyFromRatio(0.7))""")
-    assertDoesNotCompile("""DamageMove("Thunder", powerFromInt(120), 16, accuracyFromRatio(0.7))""")
-
+  test("Move accuracy should not be created without a value of type Power, a value of type PP and a value of type Accuracy (only PP and Accuracy for status moves)"):
+    assertDoesNotCompile("""DamageMove("Thunder", 120, 16, 70.0, Electric, Special)""")
+    assertDoesNotCompile("""DamageMove("Thunder", powerFromInt(120), powerPointsFromInt(16), 70.0, Electric, Special)""")
+    assertDoesNotCompile("""DamageMove("Thunder", 120, powerPointsFromInt(16), accuracyFromRatio(0.7), Electric, Special)""")
+    assertDoesNotCompile("""DamageMove("Thunder", powerFromInt(120), 16, accuracyFromRatio(0.7), Electric, Special)""")
+    assertDoesNotCompile("""StatusMove("Thunder wave", 32, 90, Electric)""")
+    assertDoesNotCompile("""StatusMove("Thunder wave", powerPointsFromInt(32), 90, Electric)""")
+    assertDoesNotCompile("""StatusMove("Thunder wave", 32, accuracyFromPercent(90), Electric)""")
+    
   test("Moves should be created only with stats valid values"):
-    assertThrows[IllegalArgumentException](DamageMove("Thunder", powerFromInt(251), powerPointsFromInt(67), accuracyFromRatio(1.5)))
-    assertThrows[IllegalArgumentException](DamageMove("Thunder", powerFromInt(0), powerPointsFromInt(0), accuracyFromRatio(-0.5)))
-    noException should be thrownBy DamageMove("Thunder", powerFromInt(120), powerPointsFromInt(16), accuracyFromRatio(0.7))
+    assertThrows[IllegalArgumentException](DamageMove("Thunder", powerFromInt(251), powerPointsFromInt(67), accuracyFromRatio(1.5), Electric, Special))
+    assertThrows[IllegalArgumentException](StatusMove("Thunder wave", powerPointsFromInt(0), accuracyFromRatio(-0.5), Electric))
+    noException should be thrownBy DamageMove("Thunder", powerFromInt(120), powerPointsFromInt(16), accuracyFromRatio(0.7), Electric, Special)
