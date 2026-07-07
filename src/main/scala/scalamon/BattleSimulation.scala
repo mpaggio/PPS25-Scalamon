75294c840131b06f145522426d0967ce83130031
Federico Brighi <federico.brighi2@studio.unibo.it>
2026-07-07 14:05:26 +0200

    Updated the simulation with a manual team building (still hardcoded)

--- src/main/scala/scalamon/BattleSimulation.scala
+++ src/main/scala/scalamon/BattleSimulation.scala
@@ -1,16 +1,17 @@
 package scalamon
 
 import scalamon.domain.moves.MoveDatabase
-import scalamon.domain.moves.MoveDatabase.findByName
+import scalamon.domain.moves.MoveDatabase.{allMoves, findByName}
+import scalamon.domain.pokemon.Pokemon
 import scalamon.domain.pokemon.pokedex.MyPokedex
-import scalamon.logics.state.BattleStateImpl.{BattleState, battleState}
+import scalamon.domain.pokemon.pokedex.MyPokedex.allPokemons
+import scalamon.logics.state.BattleStateImpl.BattleState
 import scalamon.logics.state.{DamagePolicy, PlayerStateModuleImpl}
 import scalamon.logics.state.StateTransformerModuleImpl.*
-import scalamon.logics.turns.BattleAction
 import scalamon.logics.turns.UseMove
-import scalamon.logics.turns.SwitchPokemon
 import scalamon.logics.turns.*
 import scalamon.logics.turns.TurnResult.*
+import scalamon.logics.teambuilder.ManualTeamBuilder.ManualTeamBuilder
 
 object BattleSimulation extends App:
 
@@ -18,23 +19,53 @@ object BattleSimulation extends App:
 
   given DamagePolicy = DamagePolicy.Medium.given_DamagePolicy
 
-  val pokedex = MyPokedex.allPokemons
-  private def findPokemon(name: String) = pokedex.find(_.name == name).get
+  private def pokemonNamed(name: String): Pokemon =
+    allPokemons.find(_.name == name).getOrElse(
+      throw new NoSuchElementException(s"Pokemon with name $name not found")
+    )
 
-  private val ember = MoveDatabase.allMoves.findByName("Ember").get
-  private val flamethrower = MoveDatabase.allMoves.findByName("Flamethrower").get
-  private val surf = MoveDatabase.allMoves.findByName("Surf").get
-  private val bubbleBeam = MoveDatabase.allMoves.findByName("Bubble beam").get
+  private def moveNamed(name: String): Move =
+    allMoves.findByName(name).getOrElse(
+      throw new NoSuchElementException(s"Move with name $name not found")
+    )
 
-  private val charmander = pokemonInitialState(findPokemon("Charmander"), Map("Ember" -> moveInitialState(ember), "Flamethrower" -> moveInitialState(flamethrower)))
-  private val charizard = pokemonInitialState(findPokemon("Charizard"), Map("Flamethrower" -> moveInitialState(flamethrower)))
-  private val squirtle = pokemonInitialState(findPokemon("Squirtle"), Map("Surf" -> moveInitialState(surf), "Bubble beam" -> moveInitialState(bubbleBeam)))
-  private val blastoise = pokemonInitialState(findPokemon("Blastoise"), Map("Surf" -> moveInitialState(surf)))
+  private val team1 = List(
+    pokemonNamed("Bulbasaur"),
+    pokemonNamed("Charmander"),
+    pokemonNamed("Squirtle"),
+    pokemonNamed("Venusaur"),
+    pokemonNamed("Charizard"),
+    pokemonNamed("Blastoise")
+  )
 
-  val player1 = playerState(Map("Charmander" -> charmander, "Charizard" -> charizard), "Charmander")
-  val player2 = playerState(Map("Squirtle" -> squirtle, "Blastoise" -> blastoise), "Squirtle")
+  private val team2 = List(
+    pokemonNamed("Pikachu"),
+    pokemonNamed("Gengar"),
+    pokemonNamed("Mewtwo"),
+    pokemonNamed("Jolteon"),
+    pokemonNamed("Hypno"),
+    pokemonNamed("Arbok")
+  )
 
-  var state = battleState(player1, player2)
+  private val builder1 = ManualTeamBuilder(
+    pokemonSelector = _ => team1,
+    moveSelector = (_, _) => List(
+      moveNamed("Body slam"),
+      moveNamed("Hyper beam"),
+      moveNamed("Double edge"),
+      moveNamed("Slash")
+    )
+  )
+
+  private val builder2 = ManualTeamBuilder(
+    pokemonSelector = _ => team2,
+    moveSelector = (_, _) => List(
+      moveNamed("Swift"),
+      moveNamed("Strength"),
+      moveNamed("Recover"),
+      moveNamed("Ember")
+    )
+  )
 
   // ORCHESTRATOR
 
@@ -42,7 +73,7 @@ object BattleSimulation extends App:
 
   // HELPER UTILI
 
-  private def firstAvailableMove(ps: PlayerStateModuleImpl.PlayerState): String =
+  private def firstAvailableMove(ps: PlayerState): String =
     ps.getActive.moves
       .find((_, ms) => ms.currentPp > 0)
       .map(_._1)
@@ -56,6 +87,20 @@ object BattleSimulation extends App:
     val s = bs.self.getActive
     val o = bs.opponent.getActive
     println(s"[Turno $turn] Player1(${bs.self.activeId}) HP: ${s.currentHp} | Player2(${bs.opponent.activeId}) HP: ${o.currentHp}")
+
+  var state = BattleSetup.setupBattle(builder1, builder2)
+
+  println("TEAM PLAYER1:")
+  state.self.team.foreach { case (name, pokemonState) =>
+    println(s"- $name -> ${pokemonState.moves.keys.mkString(", ")}")
+  }
+
+  println()
+  println("TEAM PLAYER2:")
+  state.opponent.team.foreach { case (name, pokemonState) =>
+    println(s"- $name -> ${pokemonState.moves.keys.mkString(", ")}")
+  }
+  println()
 
   // LOOP DI GIOCO
 
@@ -96,35 +141,40 @@ object BattleSimulation extends App:
         println(s"$koName e' andato KO!")
         candidates.headOption match
           case Some(newActive) =>
-            println(s" Player2 manda in campo ${newActive.value}")
+            println(s"Player2 manda in campo ${newActive.value}")
             state = orchestrator.applyOpponentForcedSwitch(state, newActive)
           case None =>
             running = false
 
       case BothForcedSwitch(_, selfCandidates, opponentCandidates) =>
-        println(s"\n${state.self.getActive.species.name} e' andato KO!")
-        println(s"\n${state.opponent.getActive.species.name} e' andato KO!")
+        println(s"${state.self.getActive.species.name} e' andato KO!")
+        println(s"${state.opponent.getActive.species.name} e' andato KO!")
+
         selfCandidates.headOption match
           case Some(newActive) =>
             println(s"Player1 manda in campo ${newActive.value}!")
             state = orchestrator.applyForcedSwitch(state, newActive)
-          case None => running = false
+          case None =>
+            running = false
+
         opponentCandidates.headOption match
           case Some(newActive) =>
-            print(s"Player2 manda in campo ${newActive.value}!")
+            println(s"Player2 manda in campo ${newActive.value}!")
             state = orchestrator.applyOpponentForcedSwitch(state, newActive)
-          case None => running = false
+          case None =>
+            running = false
 
       case SelfWins(finalState) =>
         if finalState.opponent.getActive.currentHp <= 0 then
           println(s"${finalState.opponent.getActive.species.name} e' andato KO!")
-        println(s"Player2 non ha piu' Pokemon a disposizione!")
+        println("Player2 non ha piu' Pokemon a disposizione!")
         println("PLAYER1 VINCE!")
         running = false
+
       case SelfLoses(finalState) =>
         if finalState.self.getActive.currentHp <= 0 then
           println(s"${finalState.self.getActive.species.name} e' andato KO!")
-        println(s"Player1 non ha piu' Pokemon a disposizione!")
+        println("Player1 non ha piu' Pokemon a disposizione!")
         println("PLAYER2 VINCE!")
         running = false
 
